dd1f4e79c7df70bd0d758b8a705fcc77685f59f1
Shashank Araokar <shashank.araokar@gmail.com>
2022-03-22 23:44:45 -0400

    Removed unnecessary matrix row, looks better now

--- Labs/testPDE.m
+++ Labs/testPDE.m
@@ -1,17 +1,15 @@
+clc; clear;
 S0=100; 
-Smax = 500;
-T = 0.5;
-K= 100;
-r =0.02;
-sigma=0.4;
+Smax = 3;
+T = 0.25;
+K= 1;
+r =0.05;
+sigma=0.3;
 M = 200;
-N=1000;
-dtau=T/N;
-dS = Smax/M;
-dtau<(dS)^2/(sigma^2*Smax^2)
+N = 200;
 % explicit method
-[V0, V] = Call_explicit(S0, Smax, T, K, r,sigma, M,N);
+%[V0, V] = Call_explicit(S0, Smax, T, K, r,sigma, M,N);
 % implicit method
-[V1, VV] = PDE_Implicit(S0, Smax, T, K, r,sigma, M,N);
+VV = PDE_Implicit(S0, Smax, T, K, r,sigma, M,N);
 % B-S formula
-[C,P] = blsprice(S0, K, r, T, sigma)
+[C,P] = blsprice(S0, K, r, T, sigma);
