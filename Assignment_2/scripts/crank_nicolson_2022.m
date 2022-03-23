75acd1985c56297f06de5876c80604fd6a0f13d8
Shashank Araokar <shashank.araokar@gmail.com>
2022-03-23 11:40:47 -0400

    Calculate M and its LU only once

--- Assignment_2/scripts/crank_nicolson_2022.m
+++ Assignment_2/scripts/crank_nicolson_2022.m
@@ -12,7 +12,7 @@ Smax = S1*exp((r-0.5*sigma*sigma)*T + 3*sigma*sqrt(T));
 
 % The grid division factor (can take values 1,2,4,8 etc.)
 % A higher c creates a finer grid
-c = 1; 
+c = 4; 
 
 % Create time grid using factor 'c'
 dtau = (T/25)/c; % Time-step size
@@ -43,31 +43,36 @@ alpha = zeros(1,M);
 beta = zeros(1,M);
 I = eye(M);
 
+% Calculate M_hat (only once) and the LU decomposition of (I + M_hat). 
+% As per the assignment instructions, we should avoid unnecessary
+% calculations, hence this is pulled outside the second loop.
+for i = 2:M-1
+    alpha_central(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i) - S(i-1)) ...
+        * (S(i+1) - S(i-1)))) - (r * S(i))/(S(i+1) - S(i-1));
+    beta_central(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i+1) - S(i)) ...
+        * (S(i+1) - S(i-1)))) + (r * S(i))/(S(i+1) - S(i-1));
+    alpha_forward(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i) - S(i-1)) ...
+        * (S(i+1) - S(i-1))));
+    beta_forward(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i+1) - S(i)) ...
+        * (S(i+1) - S(i-1)))) + (r * S(i))/(S(i+1) - S(i-1));
+    if (alpha_central(i) >= 0 ) && (beta_central(i) >= 0)
+        alpha(i) = alpha_central(i);
+        beta(i) = beta_central(i);
+    else 
+        alpha(i) = alpha_forward(i);
+        beta(i) = beta_forward(i);
+    end 
+end  % end of M for-loop
+vector1 = [-r*dtau/2, dtau/2 *(alpha(1:M-2) + beta(1:M-2) + r), 0];
+vector2 = [0, -dtau/2 * beta(1:M-2)];
+vector3 = [-dtau/2 * alpha(1:M-2), 0];
+M_hat = diag(vector1) + diag(vector2, 1) + diag(vector3, -1);
+% Use LU decomposition for faster results
+[L, U] = lu(I + M_hat);
+
 for n = 1:N-1
     V(n+1, 1) = V(n, 1) * ((1 + (r / 2) * dtau) / (1 - (r / 2) * dtau));
     V(n+1, M) = V(n, M);
-    for i = 2:M-1
-        alpha_central(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i) - S(i-1)) ...
-            * (S(i+1) - S(i-1)))) - (r * S(i))/(S(i+1) - S(i-1));
-        beta_central(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i+1) - S(i)) ...
-            * (S(i+1) - S(i-1)))) + (r * S(i))/(S(i+1) - S(i-1));
-        alpha_forward(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i) - S(i-1)) ...
-            * (S(i+1) - S(i-1))));
-        beta_forward(i) = ((sigma ^ 2) * (S(i) ^2))/(((S(i+1) - S(i)) ...
-            * (S(i+1) - S(i-1)))) + (r * S(i))/(S(i+1) - S(i-1));
-        if (alpha_central(i) >= 0 ) && (beta_central(i) >= 0)
-            alpha(i) = alpha_central(i);
-            beta(i) = beta_central(i);
-        else 
-            alpha(i) = alpha_forward(i);
-            beta(i) = beta_forward(i);
-        end 
-    end  % end of M for-loop
-    vector1 = [-r*dtau/2, dtau/2 *(alpha(1:M-2) + beta(1:M-2) + r), 0];
-    vector2 = [0, -dtau/2 * beta(1:M-2)];
-    vector3 = [-dtau/2 * alpha(1:M-2), 0];
-    M_hat = diag(vector1) + diag(vector2, 1) + diag(vector3, -1);
-
-    V(n+1, :) = transpose(inv(I + M_hat) *(I - M_hat)*transpose(V(n, :)));
-        
+    B = (I - M_hat)*transpose(V(n, :));
+    V(n+1, :) = transpose(U\(L\B));
 end % end of N for-loop
