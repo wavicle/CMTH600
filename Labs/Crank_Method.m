c09623dfc7dc738087098f1ff0584cbac230bacd
Shashank Araokar <shashank.araokar@gmail.com>
2022-03-22 23:44:01 -0400

    Had many errors. Fixed them. Looks perfect now!

--- Labs/Crank_Method.m
+++ Labs/Crank_Method.m
@@ -5,8 +5,8 @@ T = 0.25;
 S1 = 0;
 Smax = 3;
 K = 1;
-M = 200;
-N = 200;
+M = 50;
+N = 50;
 r = 0.05;
 sigma = 0.3;
 dS = (Smax - 0)/M;
@@ -37,7 +37,7 @@ for n = 1:N-1
             beta(i) = beta_forward(i);
         end 
            end  % end of M for-loop
-    vector1 = [dtau/2 *(alpha(1:M-1) + beta(1:M-1) - r), 0];
+    vector1 = [-r*dtau/2, dtau/2 *(alpha(1:M-2) + beta(1:M-2) + r), 0];
     vector2 = [0, -dtau/2 * beta(1:M-2)];
     vector3 = [-dtau/2 * alpha(1:M-2), 0];
     M_hat = diag(vector1) + diag(vector2, 1) + diag(vector3, -1);
@@ -45,7 +45,7 @@ for n = 1:N-1
      V(n+1, :) = transpose(inv(I + M_hat) *transpose(V(n, :))) * transpose(I - M_hat);
 end % end of N for-loop
 V(N, M-1)
-%[Call, Put] = blsprice(Smax,K, r, T, sigma)
+[Call, Put] = blsprice(Smax,K, r, T, sigma)
 
             
     
